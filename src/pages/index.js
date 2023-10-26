c8d9831ce9ffdd18588e1fc2c7d3e587960091f0
benja <benjamin.astudillo.loyer@gmail.com>
2023-10-25 23:35:15 -0300

    feat translated version for remote access

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,19 @@ import '@fontsource/rubik'
 import styled from "styled-components"
 import './styles.css'
 import { StaticImage } from "gatsby-plugin-image"
+import scrollTo from 'gatsby-plugin-smoothscroll';
+import { Helmet } from "react-helmet"
 
 // const CalendlyWidget = () => (
 //   <div dangerouslySetInnerHTML={{ __html: require('../static/calendly.html') }} />
 // );
 
 export const LogoStyled = styled.div`
-    font-size: 36px;
+    font-size: 46px;
     font-family: "Rubik", "sans-serif";
     color: black;
     padding: 8px;
+    justify-self: center;
 `
 export const Logo = () =>{
   return <LogoStyled><b>&lt;H</b><span style={{color: 'rgb(223, 123, 30)'}}>old</span><b>TML/&gt;</b></LogoStyled>
@@ -24,82 +27,95 @@ const IndexPage = () => {
     <main>
 
       <section className="headline">
-        <Logo></Logo>
-        <h1>Paquete de diseño Web para bares y restaurantes</h1>
+        <Logo className="logo-head"></Logo>
+        <h1>Web Design Package for Bars and Restaurants</h1>
 
 
         <div className="whats_the_catch">
           <div className="catch_CTA">
 
-            <h2>Sin Costo? =&gt;</h2>
+            <h2>No Cost? =&gt;</h2>
 
             <div className="scarecity">
-              <p>Tenemos cupos limitados para esta oferta, y solo trabajamos con 1 cliente a la vez, asi que si te interesa, contactanos pronto para reservar tu cupo</p>
-              <button className="redeem_btn">me interesa</button>
+            <p>We have limited slots for this offer, and we work with only one client at a time, so if you're interested, contact us soon to reserve your spot</p>
+              <button onClick={()=> scrollTo("#calendly")} className="redeem_btn">Im interested</button>
             </div>
           </div>
           
           <div className="catch_list">
-            <h3>Si, no hay costo para esta oferta mientras estes dispuesto ha</h3>
-            <ul className='catch_list_wrapper'>
-              <li className="catch_list_item">Darnos un review y testimonio honestos de tu experiencia</li>
-              <li className="catch_list_item">Contestar un formulario, para ayudarnos a mejorar nuestro servicio</li>
-            </ul>
+            <h3>Yes, there is no cost for this offer as long as you are willing to</h3>
+              <ul className='catch_list_wrapper'>
+                <li className="catch_list_item">Give us honest reviews and testimonials of your experience</li>
+                <li className="catch_list_item">Fill out a form to help us improve our service</li>
+                <li className="catch_list_item">Refer someone who is interested in our services if you are satisfied</li>
+              </ul>
           </div>
         </div>
       </section>
 
+      <div className="line-break"/>
+      
       <section className="solution">
         <div className="solution_webdev">
           <div className="title-wrapper">
-            <h2>Sitios Web programados en <Link target='_blank' to='https://gatsbyjs.com'>Gatsby</Link></h2>
+            <h2>Websites built with <Link target='_blank' to='https://gatsbyjs.com'>Gatsby</Link></h2>
             <StaticImage src="../images/icon.png" className="gatsby-logo" alt="gatsby logo"></StaticImage>
           </div>
 
-          <div className="paragraph-wrapper">
-            <StaticImage src="../images/gatsby-code.jpg" className="code-img" alt="code editor, with gatsby code"></StaticImage>
-            <p>Nuestras paginas funcionan con un modelo de generación estatica, haciendolas extremadamente rapidas y optimizadas</p>
+          <div className="paragraph-wrapper solution-block">
+            <StaticImage src="../images/ssg-host-flow.png" className="code-img" alt="code editor, with gatsby code"></StaticImage>
+            <p>Our pages operate on a static generation model, making them extremely fast and optimized</p>
           </div>
 
-          <div className='insights_wraper'>
-            <p>Verificamos optimización y velocidad con <Link target="_blac" to='https://pagespeed.web.dev'>google page speed insights</Link></p>
-            {/* <StaticImage src="" className="gmb-logo" alt="page speed insights example"></StaticImage> */}
+          <div className='insights-wrapper solution-block'>
+            <p>We verify optimization and speed with <Link target="_blac" to='https://pagespeed.web.dev'>google page speed insights</Link></p>
+            <StaticImage src="../images/google-pagespeed-insights-logo" className="insights-logo" alt="page speed insights example"></StaticImage>
           </div>
         </div>
 
         <div className="mail-prismic-wrapper">
-          <div className="solution_email">
-            <p>Manten a tus clientes al día de tus ofertas con marketing a traves de email, para mantenerlos anciosos de volver regularmente</p>
+
+          <div className="solution_prismic solution-block">
+            <StaticImage src="../images/prismic-logo.png" className="prismic-img" alt="logo prismic"></StaticImage>
+            <p>We implement <Link to="https://prismic.io" target="_blank">Prismic</Link> on our sites so you can edit site content without the need to learn programming</p>
           </div>
-          <div className="solution_gmb">
-            <p>implementamos prismic en nuestros sitios para que puedas editar el contenido del sitio sin necesidad de aprender a programar</p>
-            {/* <StaticImage src="../images/prismic-image.jpg" className="prismic-img" alt="interfaz prismic"></StaticImage> */}
+
+          <div className="solution_email solution-block">
+            <p>Keep your customers updated on your offers with email marketing to keep them eager to return regularly</p>
+            <StaticImage src="../images/mailerlite-logo" className="email-img" alt="logo email"></StaticImage>
           </div>
+
         </div>
 
-        <div className="solution_GMB">
-          <p>Optimización de perfil gmb (google my business) para optimizar busquedas locales</p>
-          {/* <StaticImage src="" className='gmb-img' ></StaticImage> */}
+        <div className="solution_GMB solution-block">
+          <StaticImage src="../images/google-my-business-logo" className='gmb-img' ></StaticImage>
+          <p>Optimize your Google My Business (GMB) profile to enhance local searches</p>
         </div>
       </section>
 
+      <div className="line-break"/>
 
       <section className="audience">
-        <h3>para quien es este servicio?</h3>
-        <p>Nuestro servicio es para bares y restaurantes en concepción y san pedro de la paz</p>
-      {/* <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d25545.783642168273!2d-73.07365301254436!3d-36.83713606286773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sbares!5e0!3m2!1sen!2scl!4v1697381978274!5m2!1sen!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+       <h3>Who is this service for?</h3>
+        <p>Our service is ideal for bars and restaurants in Concepción and San Pedro de la Paz</p>
+        <div className="map-imbed" dangerouslySetInnerHTML={{ __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d12773.719717687067!2d-73.07352979325164!3d-36.83217824284281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sbares!5e0!3m2!1ses-419!2scl!4v1697406203709!5m2!1ses-419!2scl" width="100%" height="560" style="border: 0; border-radius: 14px; box-shadow: rgba(2, 1, 0, 0.14) 0px 1px 41px 2px; margin-bottom: 10px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' }} />
       </section>
+
       <section className="identity">
-        {/* <StaticImage src="../images/carnet-pic.jpg" className="benjamin-img" ></StaticImage> */}
-        <p>Yo soy Benjamin Astudillo, in joven empresario y desarrollador web que lleva mas de 2 años estudiando y practicando para mejorar mis servicios</p>
+        <StaticImage src="../images/carnet-pic.jpeg" className="benjamin-img" ></StaticImage>
+        <p>I am Benjamin Astudillo, a young entrepreneur and web developer with over 2 years of study and practice to improve my services</p>
       </section>
-      <section className="calendly">
-        {/* <CalendlyWidget /> */}
-      {/* <!-- Calendly inline widget begin --> */}
-        {/* <div className="calendly-inline-widget" data-url="https://calendly.com/holdtml-benjamin/reunion-inicial?primary_color=df7b1e"></div>
-        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script> */}
-      {/* <!-- Calendly inline widget end --> */}
+  
+      <section id="calendly">
+        <h2>Schedule a hassle-free video call to discuss your interest in our service</h2>
+        <Helmet>
+          <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+        </Helmet>
+        <div id="calendly-imbed">
+          <div class="calendly-inline-widget" data-url="https://calendly.com/holdtml-benjamin/reunion-inicial?primary_color=df7b1e" style={{ minWidth: '320px', height: '700px', width: '100%', margin: ' 0px 0px 120px 0px', padding: '0px' }}></div>
+        </div>
       </section>
+  
     </main>
   )
 }
